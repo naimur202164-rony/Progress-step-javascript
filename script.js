179c75ecf18d00e88361ef2e0a98876c41515de4
Your Name <naimur202163@gmail.com>
2022-09-02 07:29:57 +0600

    current active added

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const prev=document.getElementById('prev');
 const next=document.getElementById('next');
 const circle=document.querySelectorAll('.circle')
 console.log(progress)
+
+
+let currentActive=1;
+next.addEventListener('click',()=>{
+    currentActive++;
+    console.log(currentActive);
+})
