17f5164ea6fd58c40ac9ec160f79f9593fbe8fb9
Your Name <naimur202163@gmail.com>
2022-09-02 07:27:33 +0600

    all items are got

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 console.log("Progrss container");
 const progress=document.getElementById('progress');
+const prev=document.getElementById('prev');
+const next=document.getElementById('next');
+const circle=document.querySelectorAll('.circle')
+console.log(progress)
