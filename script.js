432b8a5564e68ffec20a7a985c89c130a7f9d28e
Your Name <naimur202163@gmail.com>
2022-09-02 07:55:24 +0600

    colors updated

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 console.log("Progrss container");
 const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
@@ -10,38 +9,45 @@ let currentActive = 1;
 next.addEventListener("click", () => {
   currentActive++;
   console.log(currentActive);
-  if(currentActive>circle.length){
-    currentActive=circle.length
+  if (currentActive > circle.length) {
+    currentActive = circle.length;
   }
 
   console.log(currentActive);
-    update()
+  update();
 });
 
 // Working on Prev
 prev.addEventListener("click", () => {
-    currentActive--;
-    console.log(currentActive);
-    if(currentActive<1){
-      currentActive=1;
-    }
-    update()
-    console.log(currentActive);
-  });
-  
-
-  const update=()=>{
-    circle.forEach((circle,index)=>{
-        if(index<currentActive){
-            circle.classList.add('active')
-        }else{
-            circle.classList.remove('active')
-        }
-    })
-
-
-
-    const actives=document.querySelectorAll('.active')
-
-    progress.style.width=(actives.length / circle.length)*100 +'%'
+  currentActive--;
+  console.log(currentActive);
+  if (currentActive < 1) {
+    currentActive = 1;
   }
+  update();
+  console.log(currentActive);
+});
+
+const update = () => {
+  circle.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circle.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circle.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled=false;
+  }
+};
